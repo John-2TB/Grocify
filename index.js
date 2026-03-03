f9380c062495f0b8e3c7b0622d2c666612271fcc
John-2TB <johnkoto2tb@gmail.com>
2026-03-03 15:47:58 +0100

    index.js fixed

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const signupBtn = document.getElementById('signupBtn');
 const loginText = document.getElementById('loginText');
+const arrowIcon = document.getElementById('arrow-icon');
+
+let isLoginVisible = false;
 
 signupBtn.addEventListener('click', function() {
-  if (loginText.classList.contains('hidden')) {
+  if (!isLoginVisible) {
+    //Show the login text
     loginText.classList.remove('hidden');
+    arrowIcon.style.transform = 'rotate(180deg)';
     signupBtn.textContent = 'Sign Up';
+    signupBtn.appendChild(arrowIcon); // Re-append the arrow icon after changing text
+    isLoginVisible = true;
   } else {
+    //Redirect only if the login text is already visible
+    window.location.href = 'signup.html';
     loginText.classList.add('hidden');
+    arrowIcon.style.transform = 'rotate(0deg)';
     signupBtn.textContent = 'Sign Up/Login';
-    signupBtn.appendChild(document.createElement('img')).setAttribute('src', '/images/down-arrow.png');
-    signupBtn.querySelector('img').classList.add('h-6', 'inline');
+    isLoginVisible = false;
+
   }
 })
