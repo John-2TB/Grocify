72b504b48f60cb7f3f2cb35aa9f3b985c6a6df63
John-2TB <johnkoto2tb@gmail.com>
2026-03-04 13:05:01 +0100

    Local storage for sign up information activated

--- signup.js
+++ signup.js
@@ -1,8 +1,11 @@
+const nameInput = document.getElementById('name');
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword");
 const signupBtn = document.getElementById("signupBtn");
 const passwordReport = document.getElementById("passwordReport");
+
+const nameInputValue = nameInput.value()
 
 passwordReport.classList.add("text-red-600")
 
@@ -13,12 +16,28 @@ signupBtn.addEventListener("click", function(e) {
   const confirmPasswordValue = confirmPassword.value.trim();
 
   if (passwordValue !== confirmPasswordValue) {
-    passwordReport.textContent = "Password don't match. Please try again";
-    
-  } if (passwordValue.length < 8) {
-    passwordReport.textContent = "Password must at least contain 8 characters"
-  } else {
-    window.location.href = ""
+    passwordReport.textContent = "Password don't match. Please try again.";
+    return;
+  } 
+  
+  if (passwordValue.length <8) {
+    passwordReport.textContent = "Password must at least contain 8 characters.";
+    return;
   }
+
+  passwordReport.textContent = "";
+
+  //User object
+  const user = {
+    userName: nameInputValue,
+    email: emailValue,
+    password: passwordValue
+  }
+
+  //store in local storage
+  localStorage.setItem('user', JSON.stringify(user));
+
+  //Redirect
+  window.location.href = "login.html"
 
 });
