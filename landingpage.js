017e1b56d122e1c24cbe4ed8d74463d79f37c5b6
John <johnkoto2tb@gmail.com>
2026-04-24 06:55:57 -0700

    Fixed the image

--- landingpage.js
+++ landingpage.js
@@ -1,28 +1,11 @@
+const welcomeText = document.getElementById('welcomeText');
+const storedData = localStorage.getItem('user');
+const user = JSON.parse(storedData);
+
+welcomeText.textContent = "Welcome, " + user.userName.trim().split(/\s+/)[0];
+welcomeText.classList.add("text-2xl", "font-bold")
+
+
 if (localStorage.getItem('isLoggedIn') !== "true") {
   window.location.href ='login.html'
 }
-
-const welcomeText = document.getElementById('welcomeText');
-const storedData = localStorage.getItem('user');
-
-if (!storedData) {
-  window.location.href = "login.html";
-}
-
-const user = JSON.parse(storedData);
-
-const now = new Date();
-const hour = now.getHours();
-
-let greeting;
-
-if (hour < 12) {
-  greeting = "Good Morning 🌞";
-} else if (hour < 18) {
-  greeting = "Good Afternoon 🌤";
-} else {
-  greeting = "Good Evening 🌙";
-}
-
-welcomeText.textContent = greeting + ", " + user.userName.trim().split(/\s+/)[0];
-
